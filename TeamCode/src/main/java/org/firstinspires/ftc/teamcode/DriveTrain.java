0457c28284a06fabf90fc463685d1450db357cbf
Ishaan Rout <rout.ishaan@gmail.com>
2022-11-15 21:04:03 -0800

    Updated hardware mapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 //import the firstinspires package will all the necessary libraries
 
+import android.util.Log;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.ServoController;
 
 
 @TeleOp
@@ -18,10 +20,10 @@ public class DriveTrain extends LinearOpMode {
     private DcMotor LinearSlide = null;
     private Servo LeftClaw = null;
     private Servo RightClaw = null;
-//    private DcMotor LeftFrontMotor = null;
-//    private DcMotor LeftBackMotor = null;
-//    private DcMotor RightFrontMotor = null;
-//    private DcMotor RightBackMotor = null;
+    private DcMotor LeftFrontMotor = null;
+    private DcMotor LeftBackMotor = null;
+    private DcMotor RightFrontMotor = null;
+    private DcMotor RightBackMotor = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,16 +39,20 @@ public class DriveTrain extends LinearOpMode {
         LinearSlide = hardwareMap.get(DcMotor.class, "LinearSlideMotor");
         LeftClaw = hardwareMap.get(Servo.class, "LeftClaw Servo");
         RightClaw = hardwareMap.get(Servo.class, "RightClaw Servo");
-//        LeftFrontMotor = hardwareMap.get(DcMotor.class, "LeftFrontMotor");
-//        LeftBackMotor = hardwareMap.get(DcMotor.class, "LeftBackMotor");
-//        RightFrontMotor = hardwareMap.get(DcMotor.class, "RightFrontMotor");
-//        RightBackMotor = hardwareMap.get(DcMotor.class, "RightBackMotor");
-//
-//        LeftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
-//        LeftBackMotor.setDirection(DcMotor.Direction.REVERSE);
-//        RightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
-//        RightBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        LeftFrontMotor = hardwareMap.get(DcMotor.class, "LeftFrontMotor");
+        LeftBackMotor = hardwareMap.get(DcMotor.class, "LeftBackMotor");
+        RightFrontMotor = hardwareMap.get(DcMotor.class, "RightFrontMotor");
+        RightBackMotor = hardwareMap.get(DcMotor.class, "RightBackMotor");
+
+        LeftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        LeftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        RightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        RightBackMotor.setDirection(DcMotor.Direction.REVERSE);
         LinearSlide.setDirection(DcMotor.Direction.FORWARD);
+        LeftClaw.setDirection(Servo.Direction.FORWARD);
+        RightClaw.setDirection(Servo.Direction.FORWARD);
+
+
 
         // get IMU from hardware map, and set parameters to gather data
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -62,24 +68,23 @@ public class DriveTrain extends LinearOpMode {
 
             boolean finger = false;
 
+
             if (gamepad1.right_bumper)
-                LinearSlide.setPower(0.2);
+                LinearSlide.setPower(0.3);
             else if (gamepad1.left_bumper)
-                LinearSlide.setPower(-0.2);
+                LinearSlide.setPower(-0.3);
             else
                 LinearSlide.setPower(0);
 
-//            if (gamepad1.left_trigger)
-//            {
-//                LeftClaw.setPosition();
-//                RightClaw.setPosition();
-//            }
-//
-//            if (gamepad1.right_trigger)
-//            {
-//                LeftClaw.setPosition();
-//                RightClaw.setPosition();
-//            }
+
+            if (gamepad1.a) {
+                LeftClaw.setPosition(0);
+                RightClaw.setPosition(0);
+            }
+            else if (gamepad1.b) {
+                LeftClaw.setPosition(1);
+                RightClaw.setPosition(1);
+            }
 
             double y = -gamepad1.left_stick_x; // Remember, this is reversed!
             double x = gamepad1.left_stick_y * 1.1; // Counteract imperfect strafing
@@ -91,19 +96,17 @@ public class DriveTrain extends LinearOpMode {
             double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
             double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers main    LeftFrontMotor.setPower(frontLeftPower);
-            ////            LeftBackMotor.setPower(backLeftPower);
-            ////            RightFrontMotor.setPower(frontRightPower);
-            ////        tain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
+
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
-//            RightBackMotor.setPower(backRightPower);
+            LeftFrontMotor.setPower(frontLeftPower);
+            LeftBackMotor.setPower(backLeftPower);
+            RightFrontMotor.setPower(frontRightPower);
+            RightBackMotor.setPower(backRightPower);
         }
     }
 }
